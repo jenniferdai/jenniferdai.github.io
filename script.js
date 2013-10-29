153313966df1ce26af8795fe0b41e82acfc050f6
jenniferdai <jennifer.dai@berkeley.edu>
2013-10-29 12:24:58 -0700

    fix script variable

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 for(var i = 0; i < 1000; i++) {
-    var block = $('<div class="block" />');
-    block.addClass('block-' + ((i%2)+1));
+    var block = $('<div class="resume-bt" />');
+    block.addClass('resume-bt-' + ((i%2)+1));
 
     $('body').append(block);
 }
