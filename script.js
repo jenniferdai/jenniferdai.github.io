bce5cc7df4bf629f35d2dde26878fd9c0a5ecf9b
jenniferdai <jennifer.dai@berkeley.edu>
2013-11-26 13:08:29 -0800

    Testing Jquery

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-for(var i = 0; i < 1000; i++) {
-    var block = $('<div class="resume-bt" />');
-    block.addClass('resume-bt-' + ((i%2)+1));
-
-    $('body').append(block);
-}
+console.log("Will be updated");
+$(document).ready(function() {
+	$("a.minigame").click(function() {
+		console.log("Will be updated");
+	})
+});
