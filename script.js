11c8df9a17ae568cd4730d5255c79310860ca86b
Jennifer Dai <jennifah.hai.dai@gmail.com>
2014-12-19 10:58:59 -0800

    Changed link colors so that visited doesn't change colors

--- script.js
+++ script.js
@@ -12,14 +12,6 @@ $(document).ready(function() {
 	$("#playshow").hide(); //play button
 	$("#runshow").hide(); //run button
 	$(".aboutshow").hide();
-//play button click event
-	$(".minigame").click(function() {
-		$("#resumeshow").hide();
-		$("#runshow").hide();
-		$("#projshow").hide();
-		$(".aboutshow").hide();
-		$("#playshow").fadeIn(2000);
-	})
 //resume button click event
 	$(".resume").click(function() {
 		$("#playshow").hide();
@@ -52,7 +44,5 @@ $(document).ready(function() {
 		$("#runshow").hide();
 		$(".aboutshow").fadeIn(2000);
 	})
-
-
 })
 */
